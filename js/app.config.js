8a1dd75c74b83c8871bb8ed245d9adddbe1b0b82
DigitalHawk1 <antanaslstanys@gmail.com>
2018-03-14 16:09:21 +0200

    Added english translation. Activated contact form with Formspree.

---
+++ js/app.config.js
@@ -0,0 +1,74 @@
+(function () {
+  'use strict'
+  angular.module('app')
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+      function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $locationProvider.hashPrefix('')
+        $locationProvider.html5Mode(true)
+
+        $stateProvider
+          .state('main', {
+            url: '/',
+            views: {
+              '': {
+                templateUrl: './views/main.html'
+              },
+              'footer': {
+                template: 'Mano rankų darbas © 2018 - Klaipėda'
+              }
+            }
+          }).state('main-en', {
+            url: '/en',
+            views: {
+              '': {
+                templateUrl: './views/en.main.html'
+              },
+              'footer': {
+                template: 'Handmade by me © 2018 - Klaipėda'
+              }
+            }
+          }).state('forum', {
+            url: '/forum',
+            views: {
+              '': {
+                templateUrl: './views/forum.html'
+              },
+              'footer': {
+                template: 'Mano rankų darbas © 2018 - Klaipėda'
+              }
+            }
+          }).state('forum-en', {
+            url: '/forum-en',
+            views: {
+              '': {
+                templateUrl: './views/en.forum.html'
+              },
+              'footer': {
+                template: 'Handmade by me © 2018 - Klaipėda'
+              }
+            }
+          }).state('portfolio', {
+            url: '/portfolio',
+            views: {
+              '': {
+                templateUrl: './views/portfolio.html'
+              },
+              'footer': {
+                template: 'Mano rankų darbas © 2018 - Klaipėda'
+              }
+            }
+          }).state('portfolio-en', {
+            url: '/portfolio-en',
+            views: {
+              '': {
+                templateUrl: './views/en.portfolio.html'
+              },
+              'footer': {
+                template: 'Handmade by me © 2018 - Klaipėda'
+              }
+            }
+          })
+
+        $urlRouterProvider.otherwise('/')
+      }])
+})()
